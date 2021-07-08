64b66c1d2fba09ace72f8cdb95a0373430489adf
stephenlucas600 <StephenLucas600@gmail.com>
2021-07-08 16:18:41 -0400

    Delete src/components/table directory

--- src/components/table/RenderTableHeader.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-// Render row Headers in table
-export default function renderTableHeader(dataTable) {
-  let header = Object.keys(dataTable[0]);
-  return header.map((key, index) => {
-    return <th key={index}>{key.toUpperCase()}</th>;
-  });
-}
